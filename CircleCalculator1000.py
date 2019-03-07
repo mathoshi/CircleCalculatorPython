d0343f501aa80fe51ba07fa790deec0f14245a29
Matsuru Hoshi <matsuru.hoshi@ocsbstudent.ca>
2019-03-06 21:54:37 -0500

    Added try/except

--- CircleCalculator1000.py
+++ CircleCalculator1000.py
@@ -1,15 +1,22 @@
 #Created by Matsuru Hoshi
 #Created on March 6, 2019
 #This file will print , "Hello, World!", to the CMD
-
-welcome = input("Welcome to the Circle Calculator 1000. Respond Ok if Ok: ")
-diameter = int(input("What is the diameter? "))
-
 import math
 
-circumference = 2 * (math.pi * (diameter/2))
-area = math.pi * (diameter/2) ** 2
-print( "The circumference is " + str(circumference))
-print( "The area is " + str(area))
+try:
+    name = ""
+    print("Welcome to the Circle Calculator 1000!")
+    name = raw_input("What is your name? ")
+    print( "Hello, " + str(name) + ". You opened the Circle Calculator 1000!")
+    diameter = int(input("What is the diameter? "))
 
-end = input( "Thank you!")
+    circumference = 2 * (math.pi * (diameter/2))
+    area = math.pi * (diameter/2) ** 2
+    print( "The circumference is " + str(circumference))
+    print( "The area is " + str(area))
+
+    end = input( "Thank you!")
+except NameError as err:
+    print("Bad input: %s" % err)
+except SyntaxError as err:
+    print("Bad input")
